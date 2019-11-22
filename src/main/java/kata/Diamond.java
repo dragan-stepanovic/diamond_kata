921d2990aaa3fbfcde359ff10b797ea8cc1c244b
dragan.stepanovic <dragan.stepanovic@careem.com>
2019-11-22 22:08:16 +0100

    method renamed

--- src/main/java/kata/Diamond.java
+++ src/main/java/kata/Diamond.java
@@ -15,13 +15,13 @@ public class Diamond {
 
   public static String forLetter(String widestLetter) {
     String diamond = rowFor(widestLetter);
-    for (String letter : inReverseStartingFrom(widestLetter)) {
+    for (String letter : lettersInReverseStartingFrom(widestLetter)) {
       diamond = rowFor(letter, widestLetter) + diamond + rowFor(letter, widestLetter);
     }
     return removeLastNewLineFrom(diamond);
   }
 
-  private static List<String> inReverseStartingFrom(String widestLetter) {
+  private static List<String> lettersInReverseStartingFrom(String widestLetter) {
     List<String> reversedLetters = listUntil(widestLetter);
     Collections.reverse(reversedLetters);
     return reversedLetters;
